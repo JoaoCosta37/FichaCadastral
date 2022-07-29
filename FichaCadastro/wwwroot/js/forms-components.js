f9b0c51261bb174aea720f95c78839d53566122d
JoaoCosta37 <109122261+JoaoCosta37@users.noreply.github.com>
2022-07-29 15:04:16 -0300

    Inicio da Aplicação das Máscaras

--- FichaCadastro/wwwroot/js/forms-components.js
+++ FichaCadastro/wwwroot/js/forms-components.js
@@ -7,7 +7,7 @@
     $("#tabstrip").kendoTabStrip({
         animation: {
             open: {
-                effects: "fadeIn"
+                effects: "fadeOn"
             }
         }
     });
@@ -18,7 +18,7 @@
     // Date START
     // Date START
     // create DatePicker from input HTML element
-    $("#datepicker").kendoDatePicker();
+    
 
     $("#monthpicker").kendoDatePicker({
         // defines the start view
@@ -60,14 +60,6 @@
         mask: "L0L 0LL"
     });
 
-    $("#rg").kendoMaskedTextBox({
-        mask: "## ### ### - #"
-    });
-
-    $("#cpf").kendoMaskedTextBox({
-        mask: "### ### ### - ##"
-    });
-
     // Masked Text END
     // Masked Text END
 
@@ -88,6 +80,81 @@
         suggest: true,
         index: 0
     });
+
+    $('#nome').kendoMaskedTextBox({
+        value:familiaData.Responsavel.Nome,
+        //limitando a 100 caracteres
+        mask: "????????????????????????????????????????????????????????????????????????????????????????????????????"
+    });
+
+    $('#rg').kendoMaskedTextBox({
+        value:familiaData.Rg,
+        //limitando a  caracteres
+        mask: ""
+    });
+    $('#cpf').kendoMaskedTextBox({
+        value:familiaData.Cpf,
+        //limitando a  caracteres
+        mask: ""
+    });
+    $("#data-nascimento").kendoDatePicker({
+        value:familiaData.Responsavel.DataNascimento,
+    });
+    $('#phone_number').kendoMaskedTextBox({
+        value:familiaData.Celular,
+        //limitando a  caracteres
+        mask: ""
+    });
+    $('#tel_number').kendoMaskedTextBox({
+        value:familiaData.Telefone,
+        //limitando a  caracteres
+        mask:""
+    });
+    $('#sexoMasculino').kendoMaskedTextBox({
+        value:familiaData.Sexo,
+        //limitando a  caracteres
+        mask: ""
+    });
+    $('#batismo').kendoMaskedTextBox({
+        value:familiaData.Batizado,
+        //limitando a  caracteres
+        mask: ""
+    });
+    $('#crisma').kendoMaskedTextBox({
+        value:familiaData.Crismado,
+        //limitando a  caracteres
+        mask: ""
+    });
+    $('#eucaristia').kendoMaskedTextBox({
+        value:familiaData.Eucaristia,
+        //limitando a  caracteres
+        mask: ""
+    });
+    $('#matrimonio').kendoMaskedTextBox({
+        value:familiaData.Matrimonio,
+        //limitando a  caracteres
+        mask: ""
+    });
+    $('#profissao').kendoMaskedTextBox({
+        value:familiaData.Profissao,
+        //limitando a  caracteres
+        mask: ""
+    });
+    $('#estado-civil').kendoMaskedTextBox({
+        value:familiaData.EstadoCivil,
+        //limitando a  caracteres
+        mask: ""
+    });
+    $('#situacao-emprego').kendoMaskedTextBox({
+        value:familiaData.SituacaoEmprego.Id,
+        //limitadatando a  caracteres
+        mask: ""
+    });
+
+
+
+
+
 
     $('#endereco').kendoMaskedTextBox({
         value: familiaData.Endereco,
